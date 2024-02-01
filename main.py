ded1d2c0fe0b6b857dd8a540a894ef3c4fbf72d8
singhadi672 <70839024+singhadi672@users.noreply.github.com>
2024-02-01 15:40:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
 from fastapi import FastAPI, Response, status
 from models import studentSchema,booksSchema
 from bson.objectid import ObjectId
+from fastapi.responses import HTMLResponse
 
 from dbConnection import database
 
 app = FastAPI()
+
+html = f"""
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>FastAPI on Vercel</title>
+        <link rel="icon" href="/static/favicon.ico" type="image/x-icon" />
+    </head>
+    <body>
+        <div class="bg-gray-200 p-4 rounded-lg shadow-lg">
+            <h1>Hello from FastAPI@{__version__}</h1>
+            <ul>
+                <li><a href="/docs">/docs</a></li>
+                <li><a href="/redoc">/redoc</a></li>
+            </ul>
+            <p>Powered by <a href="https://vercel.com" target="_blank">Vercel</a></p>
+        </div>
+    </body>
+</html>
+"""
+
+@app.get("/")
+async def root():
+    return HTMLResponse(html)
 
 @app.get("/students")
 async def get_all_student_details():
